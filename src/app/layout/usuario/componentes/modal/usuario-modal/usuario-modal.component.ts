2453fab9961c8f670fc04427a127baf8eb1b1eb2
Jean Junior Heldt <jean.heldt@gmail.com>
2021-05-29 16:56:12 -0300

    Update usuario-modal.component.ts

--- src/app/layout/usuario/componentes/modal/usuario-modal/usuario-modal.component.ts
+++ src/app/layout/usuario/componentes/modal/usuario-modal/usuario-modal.component.ts
@@ -1,37 +1,98 @@
-import { Usuario } from './../../../models/usuario.model';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { hasErrors, validateAllFormFields } from '../shared/helpers/iu.helper';
+import { UsuarioService } from '../shared/services/usuario.service';
+import { Usuario } from './usuario.model';
 
 @Component({
-  selector: 'app-usuario-modal',
-  templateUrl: './usuario-modal.component.html',
-  styleUrls: ['./usuario-modal.component.scss']
+  selector: 'app-form-usuario',
+  templateUrl: './form-usuario.component.html',
+  styleUrls: ['./form-usuario.component.scss']
 })
-export class UsuarioModalComponent implements OnInit {
+export class FormUsuarioComponent implements OnInit {
 
-  @Input()
-  usuario: Usuario | undefined;
+  formGroup?: FormGroup;
+  usuario?: Usuario;
 
-  @Output()
-  onSave: EventEmitter<Usuario> = new EventEmitter<Usuario>();
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService,
+    private usuarioService: UsuarioService) {
 
-  constructor(private activeModal: NgbActiveModal) { }
+  }
 
   ngOnInit(): void {
+    const usuario: any = {};
+    this.createForm(usuario);
   }
 
-  public salvar(): void {
-    // Valida se o formulário está valido
+  private createForm(usuario: Usuario) {
+    this.formGroup = this.formBuilder.group({
+      username: [
+        { value: usuario.username, disabled: usuario.id !== undefined },
+        Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(30)])
+      ],
+      password: [
+        usuario.password,
+        Validators.compose([Validators.required, Validators.minLength(5)])
+      ],
+      name: [
+        usuario.name,
+        Validators.compose([Validators.required, Validators.minLength(5)])
+      ],
+      email: [
+        usuario.email,
+        Validators.compose([Validators.required, Validators.email])
+      ]
+    });
 
-
-    // Chama o service do usuário para salvar
-    const newUsuario: any = {};
-
-    // Emite o evento que salvou com sucesso e passa o usuário que retornou do serviço atualizado
-    this.onSave.emit(newUsuario);
-
-    // Fecha o modal
-    this.activeModal.close();
+    this.usuario = usuario;
   }
+
+  salvar() {
+    if (this.formGroup?.invalid) {
+      this.toastr.error('Campos inválidos ou não preenchidos!');
+      validateAllFormFields(this.formGroup);
+      return;
+    }
+
+    const usuarioForm = this.formGroup?.getRawValue() as Usuario;
+    const usuario = { ...this.usuario, ...usuarioForm };
+
+    try {
+      // Chama o service para salvar na API
+      const userSaved = this.usuarioService.salvar(usuario)
+        .subscribe(result => {
+          this.toastr.success(`Usuário ${result.name} salvo com sucesso`, 'Ok');
+        }, error => {
+          this.toastr.error(error.message);
+        }, () => {
+
+        });
+
+    } catch (error) {
+      this.toastr.error(error.message);
+    }
+
+  }
+
+  get username() {
+    return this.formGroup?.get('username');
+  }
+
+  get password() {
+    return this.formGroup?.get('password');
+  }
+
+  get name() {
+    return this.formGroup?.get('name');
+  }
+
+  get email() {
+    return this.formGroup?.get('email');
+  }
+
+  hasErrors = hasErrors;
 
 }
