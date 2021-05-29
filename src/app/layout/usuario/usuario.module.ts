9d06e07464497761acadab91dab521ae27231743
Jean Junior Heldt <jean.heldt@gmail.com>
2021-05-29 16:57:07 -0300

    Update usuario.module.ts

--- src/app/layout/usuario/usuario.module.ts
+++ src/app/layout/usuario/usuario.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UsuarioComponent } from './usuario.component';
 import { UsuarioRoutingModule } from './usuario-routing.module';
 import { UsuarioModalComponent } from './componentes/modal/usuario-modal/usuario-modal.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   imports: [
     FormsModule,
     CommonModule,
-    UsuarioRoutingModule
+    UsuarioRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+    ToastrModule
   ],
   declarations: [
     UsuarioComponent,
